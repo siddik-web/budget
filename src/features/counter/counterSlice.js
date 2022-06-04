9153ce34c1b839ebc6268777aa57f30d5f87d8df
Md Siddiqur Rahman <siddikcoder@gmail.com>
2022-06-04 21:51:27 +0600

    update counter slice

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -12,7 +12,7 @@ export const counterSlice = createSlice({
             state.count +=1;
         },
         decrement: (state) => {
-            state.count -=1;
+            state.count > 0 ? state.count -= 1 :  state.count = 0;
         },
     }
 });
