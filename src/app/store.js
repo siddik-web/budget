710597c0e61b656f87d50c4a7fbc4d14ca1bf940
Md Siddiqur Rahman <siddikcoder@gmail.com>
2022-06-18 19:11:55 +0600

    add new entry form

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from '../features/counter/counterSlice';
 import incomeReducer from '../features/income/incomeSlice';
+import entryReducer from '../features/entry/entrySlice';
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
         income: incomeReducer,
+        entry: entryReducer,
     }
 });
